168b5aaa75f7a87599b858a7d5a035edc441c683
DFischer <me@danielfischer.com>
2015-11-05 01:04:03 -0800

    git

--- package.js
+++ package.js
@@ -4,7 +4,7 @@ Package.describe({
 	// Brief, one-line summary of the package.
 	summary: 'Meteor package of JedWatson/classnames',
 	// URL to the Git repository containing the source code for this package.
-	git: '',
+	git: 'https://github.com/tuulbox/classnames',
 	// By default, Meteor will default to using README.md for documentation.
 	// To avoid submitting documentation, set this field to null.
 	documentation: 'README.md'

ff132bc20046463e65874a8057b6dfe853d1f16e
DFischer <me@danielfischer.com>
2015-11-05 01:03:04 -0800

    Meteor packaged fork

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
+Meteor packaged fork
+
 Classnames
 ===========
 
--- bind.js
+++ bind.js
@@ -1,48 +1,34 @@
 /*!
-  Copyright (c) 2015 Jed Watson.
-  Licensed under the MIT License (MIT), see
-  http://jedwatson.github.io/classnames
-*/
+ Copyright (c) 2015 Jed Watson.
+ Licensed under the MIT License (MIT), see
+ http://jedwatson.github.io/classnames
+ */
 /* global define */
 
-(function () {
-	'use strict';
+var hasOwn = {}.hasOwnProperty;
 
-	var hasOwn = {}.hasOwnProperty;
 
-	function classNames () {
-		var classes = '';
+classNames = function() {
+	var classes = '';
 
-		for (var i = 0; i < arguments.length; i++) {
-			var arg = arguments[i];
-			if (!arg) continue;
+	for (var i = 0; i < arguments.length; i++) {
+		var arg = arguments[i];
+		if (!arg) continue;
 
-			var argType = typeof arg;
+		var argType = typeof arg;
 
-			if (argType === 'string' || argType === 'number') {
-				classes += ' ' + (this && this[arg] || arg);
-			} else if (Array.isArray(arg)) {
-				classes += ' ' + classNames.apply(this, arg);
-			} else if (argType === 'object') {
-				for (var key in arg) {
-					if (hasOwn.call(arg, key) && arg[key]) {
-						classes += ' ' + (this && this[key] || key);
-					}
+		if (argType === 'string' || argType === 'number') {
+			classes += ' ' + (this && this[arg] || arg);
+		} else if (Array.isArray(arg)) {
+			classes += ' ' + classNames.apply(this, arg);
+		} else if (argType === 'object') {
+			for (var key in arg) {
+				if (hasOwn.call(arg, key) && arg[key]) {
+					classes += ' ' + (this && this[key] || key);
 				}
 			}
 		}
-
-		return classes.substr(1);
 	}
 
-	if (typeof module !== 'undefined' && module.exports) {
-		module.exports = classNames;
-	} else if (typeof define === 'function' && typeof define.amd === 'object' && define.amd) {
-		// register as 'classnames', consistent with npm package name
-		define('classnames', function () {
-			return classNames;
-		});
-	} else {
-		window.classNames = classNames;
-	}
-}());
+	return classes.substr(1);
+};
